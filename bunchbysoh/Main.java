fd51e5c9bce4cad9fd374e344140e4fb22424fa2
UTKARSH <86353517+dhecode@users.noreply.github.com>
2023-12-03 17:53:54 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -1,28 +1,56 @@
 package bunchbysoh;
 
 public class Main {
-  static class CountsBySoH {
-    public int healthy = 0;
-    public int exchange = 0;
-    public int failed = 0;
-  };
+    static class CountsBySoH {
+        public int healthy = 0;
+        public int exchange = 0;
+        public int failed = 0;
+    }
 
-  static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
-    CountsBySoH counts = new CountsBySoH();
-    return counts;
-  }
+    static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
+        CountsBySoH counts = new CountsBySoH();
 
-  static void testBucketingByHealth() {
-    System.out.println("Counting batteries by SoH...\n");
-    int[] presentCapacities = {113, 116, 80, 95, 92, 70};
-    CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-    assert(counts.healthy == 2);
-    assert(counts.exchange == 3);
-    assert(counts.failed == 1);
-    System.out.println("Done counting :)\n");
-  }
+        int ratedCapacity = 120;  
 
-  public static void main(String[] args) {
-    testBucketingByHealth();
-  }
+        HashMap<String, Integer> classificationCounts = new HashMap<>();
+
+     
+        for (int presentCapacity : presentCapacities) {
+        
+            double soh = (double) presentCapacity / ratedCapacity * 100;
+
+            if (soh > 80 && soh <= 100) {
+                classificationCounts.put("healthy", classificationCounts.getOrDefault("healthy", 0) + 1);
+                System.out.println("The converion of rated capacity " +presentCapacity+" is "+soh+" and is healthy");
+            } else if (soh >= 62 && soh <= 80) {
+                classificationCounts.put("exchange", classificationCounts.getOrDefault("exchange", 0) + 1);
+                System.out.println("The converion of rated capacity " +presentCapacity+" is "+soh+" and is exchange");
+            } else if (soh < 62) {
+                classificationCounts.put("failed", classificationCounts.getOrDefault("failed", 0) + 1);
+                  System.out.println("The converion of rated capacity " +presentCapacity+" is "+soh+" and is failed");
+            }
+        }
+
+        counts.healthy = classificationCounts.getOrDefault("healthy", 0);
+        counts.exchange = classificationCounts.getOrDefault("exchange", 0);
+        counts.failed = classificationCounts.getOrDefault("failed", 0);
+        return counts;
+    }
+
+    static void testBucketingByHealth() {
+        System.out.println("Counting batteries by SoH...\n");
+        int[] presentCapacities = {113, 116, 80, 95, 92, 70};
+        CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+
+        // Print the results 
+        System.out.println("Healthy: " + counts.healthy);
+        System.out.println("Exchange: " + counts.exchange);
+        System.out.println("Failed: " + counts.failed);
+
+        System.out.println("\nDone counting :)\n");
+    }
+
+    public static void main(String[] args) {
+        testBucketingByHealth();
+    }
 }
